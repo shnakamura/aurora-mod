f00ec598a28d2e70e415be884b548e87d1b7affc
shnakamura <joaovtnakmura@gmail.com>
2024-08-14 00:05:45 -0300

    Base project files

---
+++ Aurora.cs
@@ -0,0 +1,3 @@
+namespace Aurora;
+
+public sealed class Aurora : Mod;
---
+++ Usings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.Xna.Framework;
+global using Microsoft.Xna.Framework.Graphics;
+global using System;
+global using Terraria;
+global using Terraria.ModLoader;
