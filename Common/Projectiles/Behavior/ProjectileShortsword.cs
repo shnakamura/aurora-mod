4afc1a84a835afe0ade47a2c8ee5d8ed72de46fc
shnakamura <joaovtnakmura@gmail.com>
2024-08-20 21:47:31 -0300

    Start of shortsword component

--- Common/Projectiles/Behavior/ProjectileShortsword.cs
+++ Common/Projectiles/Behavior/ProjectileShortsword.cs
@@ -1,9 +1,18 @@
 using Aurora.Common.Projectiles.Components;
+using Terraria.Enums;
 
 namespace Aurora.Common.Projectiles.Behavior;
 
+// TODO: Implementation
 public sealed class ProjectileShortsword : ProjectileComponent
 {
+    public struct ShortswordData
+    {
+        public ShortswordData() { }
+    }
+
+    public ShortswordData Data = new();
+    
     public override void AI(Projectile projectile) {
         base.AI(projectile);
         
@@ -17,6 +26,21 @@ public sealed class ProjectileShortsword : ProjectileComponent
             return;
         }
 
+        ref var timer = ref projectile.ai[0];
+    }
+
+    public override void CutTiles(Projectile projectile) {
+        base.CutTiles(projectile);
+
+        if (!Enabled) {
+            return;
+        }        
         
+        DelegateMethods.tilecut_0 = TileCuttingContext.AttackProjectile;
+        
+        var start = projectile.Center;
+        var end = start + projectile.velocity.SafeNormalize(-Vector2.UnitY) * 10f;
+        
+        Utils.PlotTileLine(start, end, projectile.width, DelegateMethods.CutTiles);
     }
 }
