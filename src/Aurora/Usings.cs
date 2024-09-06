f6fc4077a349d2f9e8346c2bab460e4cfee3a825
shnakamura <joaovtnakamura@gmail.com>
2024-09-06 20:56:51 -0300

    Removes Usings.cs

--- src/Aurora/Usings.cs
+++
@@ -1,6 +0,0 @@
-global using System;
-global using Terraria;
-global using Terraria.ID;
-global using Terraria.ModLoader;
-global using Microsoft.Xna.Framework;
-global using Microsoft.Xna.Framework.Graphics;
