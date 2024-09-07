bb3eec19e701717f50ab52a9ee339e992ccce3d9
shnakamura <joaovtnakamura@gmail.com>
2024-09-07 19:27:04 -0300

    Adds generators project

---
+++ src/Aurora.Generators/FootstepGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.CodeAnalysis;
+
+namespace Aurora.Generators
+{
+	[Generator(LanguageNames.CSharp)]
+	public sealed class FootstepGenerator : IIncrementalGenerator
+	{
+		public const string Extension = ".footstep";
+
+		public void Initialize(IncrementalGeneratorInitializationContext context) {
+			var files = context.AdditionalTextsProvider.Where(file => Path.GetExtension(file.Path) == Extension);
+			var contents = files.Select((text, token) => (Name: Path.GetFileNameWithoutExtension(text.Path), Text: text.GetText(token).ToString()));
+
+		}
+	}
+}
