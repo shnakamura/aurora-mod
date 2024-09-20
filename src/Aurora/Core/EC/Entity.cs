eebc83e4e3c1585da54660b9b7ca5e5be81666bb
shnakamura <joaovtnakamura@gmail.com>
2024-09-20 03:00:38 -0300

    Summarizes Entity.cs

--- src/Aurora/Core/EC/Entity.cs
+++ src/Aurora/Core/EC/Entity.cs
@@ -4,8 +4,14 @@ namespace Aurora.Core.EC;
 
 public struct Entity : IEntity
 {
+	/// <summary>
+	///		The unique identifier of this entity.
+	/// </summary>
 	public int Id { get; }
 
+	/// <summary>
+	///		Whether this entity is active or not.
+	/// </summary>
 	public bool Active {
 		get => EntitySystem.GetActive(Id);
 		set => EntitySystem.SetActive(Id, value);
@@ -15,18 +21,39 @@ public struct Entity : IEntity
 		Id = id;
 	}
 
+	/// <summary>
+	///		Retrieves a component of the specified type from this entity.
+	/// </summary>
+	/// <typeparam name="T">The type of the component to retrieve.</typeparam>
+	/// <returns>The instance of the component if found; otherwise, <c>default</c>.</returns>
 	public T Get<T>() where T : IComponent {
 		return ComponentSystem.Get<T>(Id);
 	}
 
+	/// <summary>
+	///		Sets the value of a component of the specified type to this entity.
+	/// </summary>
+	/// <param name="value">The value of the component.</param>
+	/// <typeparam name="T">The type of the component to set.</typeparam>
+	/// <returns>The assigned component instance.</returns>
 	public T Set<T>(T? value) where T : IComponent {
 		return ComponentSystem.Set(Id, value);
 	}
 
+	/// <summary>
+	///		Checks whether this entity has a component of the specified type or not.
+	/// </summary>
+	/// <typeparam name="T">The type of component to check.</typeparam>
+	/// <returns><c>true</c> if the component was found; otherwise, <c>false</c>.</returns>
 	public bool Has<T>() where T : IComponent {
 		return ComponentSystem.Has<T>(Id);
 	}
 
+	/// <summary>
+	///		Attempts to remove a component of the specified type from this entity.
+	/// </summary>
+	/// <typeparam name="T">The type of the component to remove.</typeparam>
+	/// <returns><c>true</c> if the component was successfully removed; otherwise, <c>false</c>.</returns>
 	public bool Remove<T>() where T : IComponent {
 		return ComponentSystem.Remove<T>(Id);
 	}
